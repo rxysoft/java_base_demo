6634ebb075ebdf005ac003ef02e8f9d0b650bb1b
lxm <459734103@qq.com>
2019-05-23 17:20:36 +0800

    无污染是否

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("12345666667777");
+        System.out.println("1234566666");
+        System.out.println("111111");
     }
 }
